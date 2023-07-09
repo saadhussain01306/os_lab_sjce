e4cf9ab66ffe8231e9e9c8a42c0ab0cc4e4f6971
SAAD HUSSAIN <118361679+saadhussain01306@users.noreply.github.com>
2023-07-10 00:58:58 +0530

    Add files via upload

---
+++ lab2/fibo_series.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Program to print the fibonacci series.
+echo "Enter the number of fibonacci numbers to be printed: "
+read n
+f1=0
+f2=1
+f3=0
+echo "The first $n fibonacci numbers are: "
+for ((i=1; i<=$n; i++))
+do
+    echo "   $f3"
+    f1=$f2
+    f2=$f3
+    f3=$((f1+f2))
+done
